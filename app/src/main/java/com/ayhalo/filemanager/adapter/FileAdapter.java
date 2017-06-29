e48886cda2d9ef1bb254a909c349de6549e3a8c4
Halo <wuyihuangw@gmail.com>
2017-06-29 17:55:37 +0800

    adapter

---
+++ app/src/main/java/com/ayhalo/filemanager/adapter/FileAdapter.java
@@ -0,0 +1,63 @@
+package com.ayhalo.filemanager.adapter;
+
+import android.database.DataSetObserver;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ *
+ * Created by Halo on 2017/6/29.
+ */
+
+public class FileAdapter extends BaseAdapter{
+    @Override
+    public void registerDataSetObserver(DataSetObserver observer) {
+
+    }
+
+    @Override
+    public void unregisterDataSetObserver(DataSetObserver observer) {
+
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return 0;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
